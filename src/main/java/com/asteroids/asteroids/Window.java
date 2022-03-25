8c0050af591ac9baa92fb29ba1c5805552711551
ClarenceWhite <48053038+ClarenceWhite@users.noreply.github.com>
2022-03-25 16:34:59 +0000

    a basic window for the game, it's under Asteroids/src/main/java/'com/asteroids/asteroids'/Window.java

---
+++ src/main/java/com/asteroids/asteroids/Window.java
@@ -0,0 +1,28 @@
+package com.asteroids.asteroids;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
+public class Window extends Application{
+    //override class method 'start' to create a new window for the game
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPrefSize(1000, 1000);
+
+        Scene scene = new Scene(pane);
+        scene.setFill(Color.BLACK);
+        stage.setScene(scene);
+        stage.show();
+    }
+    // main method to launch the window
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
